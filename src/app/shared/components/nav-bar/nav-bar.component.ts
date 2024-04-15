852ba6a7fbe817ba079e76aca1e3eaccc74ab01b
Carlos <hola@cafeconcarlos.com>
2024-04-15 15:31:26 +0200

    nav bar component

---
+++ src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink, RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'nav-bar',
+  standalone: true,
+  imports: [RouterLink, RouterModule],
+  templateUrl: './nav-bar.component.html',
+  styleUrl: './nav-bar.component.css'
+})
+export class NavBarComponent {
+
+  router = inject(Router);
+
+}
