dd8ebcc1a1b2b19b77543aae248687a2603a23e1
Carlos <hola@cafeconcarlos.com>
2024-04-15 19:03:32 +0200

    hero component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './components/footer/footer.component';
+import { HeroComponent } from './components/hero/hero.component';
 
 
 import { NavBarComponent } from './shared/components/nav-bar/nav-bar.component';
@@ -8,7 +9,7 @@ import { NavBarComponent } from './shared/components/nav-bar/nav-bar.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent, NavBarComponent],
+  imports: [RouterOutlet, FooterComponent, NavBarComponent, HeroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
