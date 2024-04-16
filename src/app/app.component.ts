693fbdf02360fd09222be2f43b4f6de40a767f68
Carlos <hola@cafeconcarlos.com>
2024-04-16 15:22:05 +0200

    app-component changes following previous commits

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,13 @@ import { HeroComponent } from './components/hero/hero.component';
 
 
 import { NavBarComponent } from './shared/components/nav-bar/nav-bar.component';
+import { CardProductComponent } from './components/products/card-product/card-product.component';
+import { ListProductComponent } from './components/products/list-product/list-product.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent, NavBarComponent, HeroComponent],
+  imports: [RouterOutlet, FooterComponent, NavBarComponent, HeroComponent, CardProductComponent, ListProductComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
