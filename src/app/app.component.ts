d58fb116b896ae42d48da214ad0395dd2bad412a
Cristian Lafuente <cristian8lafuente@gmail.com>
2024-04-16 10:09:05 +0200

    The registration page is created. SweetAlert2 is installed. Defects in footer and spacer are fixed.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './components/footer/footer.component';
+import { SpacerComponent } from './components/spacer/spacer.component';
+import { RegisterComponent } from './pages/register/register.component';
+
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent],
+  imports: [RouterOutlet, FooterComponent, SpacerComponent, RegisterComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/pages/register/register.component.ts
@@ -0,0 +1,62 @@
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UsuariosService } from '../../services/usuarios.service';
+import Swal from 'sweetalert2';
+import { FooterComponent } from "../../components/footer/footer.component";
+
+@Component({
+    selector: 'app-register',
+    standalone: true,
+    templateUrl: './register.component.html',
+    styleUrl: './register.component.css',
+    imports: [FormsModule, ReactiveFormsModule, FooterComponent]
+})
+export class RegisterComponent {
+
+    formulario: FormGroup;
+
+    usuarioService = inject(UsuariosService);
+    router = inject(Router);
+
+    constructor() {
+      this.formulario = new FormGroup({
+        name: new FormControl(null, [
+          Validators.required,
+        ]),
+        last_name: new FormControl(null, [
+          Validators.required,
+        ]),
+        address: new FormControl(null, [
+          Validators.required,
+        ]),
+        email: new FormControl(null, [
+          Validators.required,
+        ]),
+        card_number: new FormControl(null, [
+          Validators.required,
+        ]),
+        password: new FormControl(null, [
+          Validators.required,
+        ]),
+
+      });
+
+  }
+  
+  async onSubmit() {
+    if (this.formulario.valid) {
+      try {
+        const response = await this.usuarioService.create(this.formulario.value);  //Método de prueba a espera de poner el correspondiente
+        this.formulario.reset();
+        // this.router.navigateByUrl('/home');    //A espera de linckar correctamente
+      } catch (error) {
+        Swal.fire('Error', 'Se ha producido un error: ');
+        this.formulario.reset();
+      }
+    } else {
+      Swal.fire('Error', 'Please complete the form correctly.', 'error');
+    }
+  }
+
+}
