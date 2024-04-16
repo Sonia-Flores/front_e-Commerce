cdf0dc3bac272b66eded42b181afb50007ad9671
Cristian Lafuente <cristian8lafuente@gmail.com>
2024-04-16 16:53:53 +0200

    The user interface and the user service are created.

--- src/app/services/usuarios.service.ts
+++ src/app/services/usuarios.service.ts
@@ -11,17 +11,14 @@ import { User } from "../data/interfaces/usuario.interface";
   providedIn: 'root'
 })
 export class UsuariosService {
-  create(value: any) {
-    throw new Error('Method not implemented.');
-  }
 
   private baseUrl = 'http://localhost:3000/api/users';
 
   private httpClient = inject(HttpClient);
 
-  registro(nuevoUsuario: User) {
+  create(nuevoUsuario: User) {
     return firstValueFrom(
-      this.httpClient.post(`${this.baseUrl}/registro`, nuevoUsuario)
+      this.httpClient.post(`${this.baseUrl}/new`, nuevoUsuario)
     )
   }
 
