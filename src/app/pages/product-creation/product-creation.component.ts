79f7a5b3c9eacd66bef3465fd78f681d18dcd78f
Cristian Lafuente <cristian8lafuente@gmail.com>
2024-04-16 17:45:02 +0200

    The product-creation page has been created.

---
+++ src/app/pages/product-creation/product-creation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-creation',
+  standalone: true,
+  imports: [],
+  templateUrl: './product-creation.component.html',
+  styleUrl: './product-creation.component.css'
+})
+export class ProductCreationComponent {
+
+}
