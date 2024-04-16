1c8f8de5f7e9eaaf86936577ead22972d49dec05
Carlos <hola@cafeconcarlos.com>
2024-04-16 15:21:07 +0200

    generation of product interface

---
+++ src/app/interfaces/product.interface.ts
@@ -0,0 +1,9 @@
+export interface Product {
+    id: number;
+    title: string;
+    description: string;
+    price: number;
+    image: string;
+    featured: boolean;
+    categories_id: number
+}
