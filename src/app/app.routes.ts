94ccd098c15079c26bd1656df541ab7f08199220
Cristian Lafuente <cristian8lafuente@gmail.com>
2024-04-18 16:10:18 +0200

    Merge branch 'feature_' into develop
    # Conflicts:
    #	src/app/app.component.html
    #	src/app/app.component.ts
    #	src/app/app.routes.ts
    #	src/app/pages/login/login.component.html
    #	src/app/pages/update-product/update-product.component.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,30 @@
+
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductCreationComponent } from './pages/product-creation/product-creation.component';
+import { UpdateProductComponent } from './pages/update-product/update-product.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
 
 
 export const routes: Routes = [
     // HOME index.html
 
-    { path: '', pathMatch: 'full', redirectTo: '/index.html' },
+    { path: '', pathMatch: 'full', redirectTo: '/home'},
+    { path: 'home', component: HomeComponent },
+
+    { path: 'register', component: RegisterComponent },
+    { path: 'login', component: LoginComponent },
+
+    { path: 'products/create/new', component: ProductCreationComponent },
+    { path: 'products/update/:product_id', component: UpdateProductComponent },
+
+    { path: 'products/:idproduct', component: ProductDetailComponent},
+
+    { path: '**', redirectTo: '/home' }
+
+
 
     // api/users/register (@Cristian)
     // api/users/login (@Cristian)
@@ -15,6 +35,6 @@ export const routes: Routes = [
 
     // api/orders/new
 
-    // { path: '**', redirectTo: '/' }
+    // { path: '', redirectTo: '/' }
 
 ];
