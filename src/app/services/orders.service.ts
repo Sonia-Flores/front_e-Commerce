b9de131b955d16b8c93639cdb91d7604d8707b7e
Albert Pallach Font <albertpf@gmail.com>
2024-04-22 17:08:50 +0200

    Orders Services

---
+++ src/app/services/orders.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class OrdersService {
+    private baseUrl = 'http://localhost:3000/api/orders';
+
+    private httpClient = inject(HttpClient);
+
+    getAll() {
+        return firstValueFrom(this.httpClient.get(this.baseUrl));
+    }
+
+    getAllWithNames() {
+        return firstValueFrom(this.httpClient.get(`${this.baseUrl}/names`));
+    }
+
+    getById(order_id: number) {
+        return firstValueFrom(this.httpClient.get(`${this.baseUrl}/${order_id}`));
+    }
+
+    create(newOrder: any) {
+        return firstValueFrom(this.httpClient.post(`${this.baseUrl}/new`, newOrder));
+    }
+
+    deleteOrder(order_id: number) {
+        return firstValueFrom(this.httpClient.delete(`${this.baseUrl}/${order_id}`));
+    }
+}
