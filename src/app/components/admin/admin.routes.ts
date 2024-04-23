eb4e1f995629f429b80011cde655a22d85466f8f
Carlos <hola@cafeconcarlos.com>
2024-04-23 12:51:26 +0200

    static changes in dashboard

--- src/app/components/admin/admin.routes.ts
+++ src/app/components/admin/admin.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from "@angular/router";
+import { OrdersManagementComponent } from "../../pages/orders-management/orders-management.component";
 import { CategoryDashboardComponent } from "./category-dashboard/category-dashboard.component"
-
 import { ProductDashboardComponent } from "./product-dashboard/product-dashboard.component"
 import { MainDashboardComponent } from "./main-dashboard/main-dashboard.component";
 
@@ -8,5 +8,6 @@ import { MainDashboardComponent } from "./main-dashboard/main-dashboard.componen
 export const adminRoutes: Routes = [
     { path: '', component: MainDashboardComponent },
     { path: 'categories', component: CategoryDashboardComponent },
-    { path: 'products', component: ProductDashboardComponent }
+    { path: 'products', component: ProductDashboardComponent },
+    { path: 'orders', component: OrdersManagementComponent }
 ];
--- src/app/shared/components/nav-bar/nav-bar.component.ts
+++ src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -1,5 +1,9 @@
 import { Component, inject } from '@angular/core';
 import { Router, RouterLink, RouterModule } from '@angular/router';
+import { jwtDecode } from 'jwt-decode';
+import { User } from '../../../data/interfaces/usuario.interface';
+import { UsuariosService } from '../../../services/usuarios.service';
+
 
 @Component({
   selector: 'nav-bar',
@@ -11,5 +15,23 @@ import { Router, RouterLink, RouterModule } from '@angular/router';
 export class NavBarComponent {
 
   router = inject(Router);
+
+  usuariosService = inject(UsuariosService);
+
+  decodedToken: any = "";
+  arrOrders: any = [];
+  arrUsers: User[] = [];
+
+
+  async ngOnInit() {
+
+    if (localStorage['token']) {
+      this.decodedToken = jwtDecode(localStorage['token']);
+    }
+
+    if (!localStorage['token'] || this.decodedToken.role !== 'admin') {
+      this.router.navigateByUrl("/home");
+    }
+  }
 
 }
